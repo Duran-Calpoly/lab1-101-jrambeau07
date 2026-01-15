a08f5b057fac102e4a4109ddc8eaf1299f622ca2
Jayden Rambeau <jrambeau@calpoly.edu>
2026-01-15 02:25:40 +0000

    lab1_4 completed

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,11 @@
-pass
+def calculate_average (num1:float, num2:float, num3:float) -> float:
+    total_avg = (num1 + num2 + num3)/3
+    return total_avg
+
+def add_tax (bill_total: float) -> float:
+    tax = bill_total * .10
+    total_bill = bill_total + tax
+    return total_bill
+
+def greet_user (name: str) -> str:
+    return f"Hello {name}"
